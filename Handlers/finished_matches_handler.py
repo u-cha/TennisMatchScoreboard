309f68af4d5c0047168d87e3d0f58ffaec794280
Sergey Udachin <s.udachin@mail.ru>
2023-09-05 11:57:59 +0300

    minor refactoring: finished matches handler

--- Handlers/finished_matches_handler.py
+++ Handlers/finished_matches_handler.py
@@ -5,6 +5,29 @@ from urllib.parse import parse_qs
 from Scores.Score import ScoreSchema
 
 
+class MatchRecordsProcessingService:
+    @classmethod
+    def process(cls, records):
+        output = []
+        for record in records:
+            match = record[0]
+            score = ScoreSchema().loads(match.score)
+            player_names = cls.__get_player_names(match.player1, match.player2, PermanentDBService)
+            player1, player2 = player_names['player1_name'], player_names['player2_name']
+            winner = player_names[f'player{match.winner}_name']
+            num_sets = len(score.sets)
+            set1_results, set2_results, set3_results = [
+                f"{score.sets[n].p1_points}:{score.sets[n].p2_points}" if n < num_sets else "---" for n in range(3)]
+            output.append(dict(player1=player1, player2=player2, winner=winner,
+                               set1_results=set1_results, set2_results=set2_results, set3_results=set3_results,
+                               ))
+        return output
+
+    @staticmethod
+    def __get_player_names(player1_id, player2_id, db_service):
+        return DBService(db_service).get_player_names(player1_id, player2_id)
+
+
 class FinishedMatchesHandler(Handler):
     def perform_get(self):
         player_names_list = self.__get_player_names_list_from_db()
@@ -15,37 +38,16 @@ class FinishedMatchesHandler(Handler):
         records = self.__retrieve_matches_from_db(limit=num_rows,
                                                   offset=num_rows * (page_number - 1),
                                                   filter_by_player_name=filter_by_player_name)
-        output = []
-        for record in records:
-            match = record[0]
-            score = ScoreSchema().loads(match.score)
-            player_names = self.__get_player_names(match.player1, match.player2, PermanentDBService)
-            player1, player2 = player_names['player1_name'], player_names['player2_name']
-            winner = player_names[f'player{match.winner}_name']
-            try:
-                set1_results = f"{score.sets[0].p1_points}:{score.sets[0].p2_points}"
-            except:
-                set1_results = "---"
-            try:
-                set2_results = f"{score.sets[1].p1_points}:{score.sets[1].p2_points}"
-            except:
-                set2_results = "---"
-            try:
-                set3_results = f"{score.sets[2].p1_points}:{score.sets[2].p2_points}"
-            except:
-                set3_results = "---"
 
-            output.append(dict(player1=player1, player2=player2, winner=winner,
-                               set1_results=set1_results, set2_results=set2_results, set3_results=set3_results,
-                               ))
-
+        output = MatchRecordsProcessingService.process(records)
         body = View.render("finished_matches", output=output, player_names_list=player_names_list)
         self.response.body = body
 
     def perform_post(self):
         pass
 
-    def __retrieve_matches_from_db(self, **kwargs):
+    @staticmethod
+    def __retrieve_matches_from_db(**kwargs):
         db_service = DBService(PermanentDBService)
         matches_list = db_service.get_matches(**kwargs)
         return matches_list
@@ -59,7 +61,8 @@ class FinishedMatchesHandler(Handler):
             num_rows = 10
         return num_rows
 
-    def __get_matches_count_from_db(self):
+    @staticmethod
+    def __get_matches_count_from_db():
         db_service = DBService(PermanentDBService)
         matches_count = db_service.get_matches_count()
         return matches_count
@@ -87,11 +90,8 @@ class FinishedMatchesHandler(Handler):
                 return filter_by_player_name[0]
         return None
 
-    def __get_player_names_list_from_db(self):
+    @staticmethod
+    def __get_player_names_list_from_db():
         db_service = DBService(PermanentDBService)
         player_names_list = db_service.get_player_names_list()
         return player_names_list
-
-    @staticmethod
-    def __get_player_names(player1_id, player2_id, db_service):
-        return DBService(db_service).get_player_names(player1_id, player2_id)
